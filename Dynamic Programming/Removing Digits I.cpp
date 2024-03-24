9bb2d8f31396e83cbfa58fb71d431b9811335d68
cookieplus <dangkienthanh@gmail.com>
2024-03-24 17:06:20 +0700

    Update Removing Digits I.cpp
    Thêm phân tích lời giải

--- Dynamic Programming/Removing Digits I.cpp
+++ Dynamic Programming/Removing Digits I.cpp
@@ -1,3 +1,10 @@
+/*
+    Có thể giải bằng tham lam.
+    Hoặc giải bằng DP:
+        Với mỗi số d có nhiều chữ số cho trước, ta thử trừ đi từng chữ số KHÁC 0 của d (vì bỏ 0 đi thì d không đổi)
+        dp[i] = min(dp[i], dp[(i sau khi trừ chữ số khác 0)] + 1);
+*/
+    
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -9,9 +16,10 @@ int N, dp[maxN+1];
 int main(){
     scanf("%d", &N);
     fill(dp+1, dp+N+1, INF);
-
     for(int i = 1; i <= N; i++){
         int d = i;
+        // Với mỗi số d có nhiều chữ số cho trước, ta thử trừ đi từng chữ số KHÁC 0 của d (vì bỏ 0 đi thì d không đổi)
+        // dp[i] = min(dp[i], dp[(i sau khi trừ chữ số khác 0)] + 1);
         while(d > 0){
             if(d%10 != 0)
                 dp[i] = min(dp[i], dp[i-(d%10)]+1);
