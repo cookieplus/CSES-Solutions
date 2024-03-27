04ebc025736580950cc21ec8cce4bc3e0b9fa24c
cookieplus <dangkienthanh@gmail.com>
2024-03-28 01:10:23 +0700

    Update Minimizing Coins.cpp

--- Dynamic Programming/Minimizing Coins.cpp
+++ Dynamic Programming/Minimizing Coins.cpp
@@ -1,3 +1,23 @@
+/**
+Minimizing Coins
+================
+Đề bài:
+	Có n loại tiền xu c1, c2,... cn.
+	Đếm số cách để có tổng số tiền là x bằng số tiền xu tối thiểu.
+	In: 
+		Dòng 1: n, x
+		Dòng 2: c1, c2...cn
+
+Phân tích:
+	Để tạo tổng số tiền là x thì sẽ có n cách khác nhau từ tổng số tiền là x - c1, x-c2,... x-cn,
+	sau đó thêm đồng coin tương ứng c1, c2,... cn vào.
+	Như vậy ta chọn số nhỏ hơn trong các phương án thu được
+
+	Tạo hàm mục tiêu dp[i] là số đồng xu tối thiểu cần thiết để có tổng là i
+	dp[i] = min(dp[i], dp[i-k] + 1); với k thuộc [c1, c2,...cn]
+
+	Code dưới đây thậm chí quét chỉ 1 lần.
+*/    
 #include <bits/stdc++.h>
 
 using namespace std;
