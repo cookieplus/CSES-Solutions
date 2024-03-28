0d4f9cb39236ff1c20f0c9bc0ad5916d41f6711d
cookieplus <dangkienthanh@gmail.com>
2024-03-28 09:46:00 +0700

    Update Book Shop I.cpp

--- Dynamic Programming/Book Shop I.cpp
+++ Dynamic Programming/Book Shop I.cpp
@@ -1,3 +1,36 @@
+/**
+Book Shop
+=========
+Đề bài:
+	Có n quyển sách. Mỗi quyển có giá hi và số trang si.
+	Tìm số trang sách tối đa có thể có với số tiền nhiều nhất là x.
+	Mỗi quyển được mua 1 lần.
+	In: 
+		n, x
+		h1... hn
+		s1... sn
+	Out:
+		Số trang sách.
+	Giới hạn:
+		1 <= n <= 1000		
+		1 <= x <= 10^5
+		1 <= hi, si <= 1000
+Phân tích:
+	Gọi dp[x] là số trang sách tối đa có thể mua với x đồng.
+	Như vậy có thể có nhiều phương án, bắt đầu từ x-hi đồng, mua được dp[x-hi] trang sách, sau đó mua thêm quyển sách thứ i, có giá hi và số trang sách là si. Tổng cộng sẽ có dp[x-hi] + si.
+	dp[x] = max(dp[x], dp[x-hi]+si) với i từ 1 đến n.
+	Cuối cùng dp[x] = max(dp)
+
+Đoạn code chính
+	for(int i=0; i<N; i++) {
+		for(int j=X-h[i]; j>=0; j--) {
+			if (dp[j] != -1) {
+				dp[j+h[i]] = max(dp[j+h[i]], dp[j] + s[i]);
+			}
+		}
+	}
+	result = * max_element(dp.begin(), dp.end());
+*/   
 #include <bits/stdc++.h>
 
 using namespace std;
